e7ff911319cc12040146cb7350227b572b7da448
z5146619 <brono_25@hotmail.com>
2022-06-27 18:58:27 +1000

    rm status mostly working

--- testing/scrap.sh
+++ testing/scrap.sh
@@ -1,6 +1,30 @@
 #!/bin/dash
 PATH=$PATH:/import/cage/3/z5146619/tigger
 
+. ./test_functions.sh
+
+
+mkdir delete
+cd delete
+
+echo "-------------DELTA_IND_PWD_DELTA_REP_PWD------------"
+ref_open_tigger
+echo f > a
+
+2041 tigger-add a
+2041 tigger-rm a ;echo "Exit status: $?"
+
+close_tigger
+
+
+
+cd ..
+rm -rf delete
+
+
+
+
+
 
 
 
@@ -9,22 +33,27 @@ cd delete
 
 
 
+echo "-------------DELTA_IND_PWD_DELTA_REP_PWD------------"
+open_tigger
+echo f > a
+tigger-add a
+tigger-rm a ;echo "Exit status: $?"
 
-2041 tigger-init
-
-echo stuff > a
-
-2041 tigger-add a
-2041 tigger-commit -m message1
+close_tigger
 
 
-2041 tigger-rm --cached a
 
-2041 tigger-rm --cached a
+
+
+
+
+
+
+
+
 
 cd ..
 rm -rf delete
-
 
 
 
