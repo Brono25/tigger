eb89627642440057025ec21965d174aacc712280
z5146619 <brono_25@hotmail.com>
2022-07-09 23:46:50 +1000

    2 tests failed

--- testing/scrap1.sh
+++ testing/scrap1.sh
@@ -16,24 +16,35 @@ ref_open_tigger
 
 
 
-seq 1 7 >7.txt
-2041 tigger-add 7.txt
+seq -f "line %.0f" 1 7 >a
+seq -f "line %.0f" 1 7 >b
+seq -f "line %.0f" 1 7 >c
+seq -f "line %.0f" 1 7 >d
+2041 tigger-add a b c d
 2041 tigger-commit -m commit-0
 
 2041 tigger-branch b1
 2041 tigger-checkout b1
 
-perl -pi -e s/2/42/ 7.txt
+seq -f "line %.0f" 0 7 >a
+seq -f "line %.0f" 1 8 >b
+seq -f "line %.0f" 1 7 >e
+2041 tigger-add e
 2041 tigger-commit -a -m commit-1
 
 2041 tigger-checkout master
 
-2041 tigger-merge b1 -m merge-message
+sed -i 4d c
+seq -f "line %.0f" 0 8 >d
+seq -f "line %.0f" 1 7 >f
+2041 tigger-add f
+2041 tigger-commit -a -m commit-2
+
+2041 tigger-merge b1 -m merge1
 
 2041 tigger-log
 
 2041 tigger-status
-
 
 
 # #-----------------------------------
@@ -50,26 +61,35 @@ open_tigger
 # #-----------------------------------
 
 
-
-seq 1 7 >7.txt
-tigger-add 7.txt
+seq -f "line %.0f" 1 7 >a
+seq -f "line %.0f" 1 7 >b
+seq -f "line %.0f" 1 7 >c
+seq -f "line %.0f" 1 7 >d
+tigger-add a b c d
 tigger-commit -m commit-0
 
 tigger-branch b1
 tigger-checkout b1
 
-perl -pi -e s/2/42/ 7.txt
+seq -f "line %.0f" 0 7 >a
+seq -f "line %.0f" 1 8 >b
+seq -f "line %.0f" 1 7 >e
+tigger-add e
 tigger-commit -a -m commit-1
 
 tigger-checkout master
 
-tigger-merge b1 -m merge-message
+sed -i 4d c
+seq -f "line %.0f" 0 8 >d
+seq -f "line %.0f" 1 7 >f
+tigger-add f
+tigger-commit -a -m commit-2
+
+tigger-merge b1 -m merge1
 
 tigger-log
 
 tigger-status
-
-
 
 
 
